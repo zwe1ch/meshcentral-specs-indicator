b5f069f5fb7ae04b1ba312bb02e0434ec94e2266
Rayk Jackisch <rayk@jackisch.net>
2025-08-05 14:33:19 +0200

    Update version to 0.0.16 in config.json and modify version references in specsindicator.js

--- specsindicator.js
+++ specsindicator.js
@@ -7,7 +7,7 @@ module.exports.specsindicator = function (parent) {
   plugin.exports = ["onWebUIStartupEnd", "goPageStart", "goPageEnd"];
 
   plugin.onWebUIStartupEnd = function () {
-    const ver = 15;
+    const ver = 16;
     console.log("onWebUIStartupEnd: " + ver);
   };
 
@@ -32,9 +32,23 @@ module.exports.specsindicator = function (parent) {
   };
 
   plugin.goPageEnd = function (index, event) {
-    const ver = 15;
-    console.log("goPageEnd: " + ver);
-    console.log("index: " + index);
+    console.log(`goPageEnd(index: ${index}, event: ${event})`);
+
+    if (index !== 1) {
+      return;
+    }
+
+    const table = document.querySelector("#xdevices > table");
+
+    console.log(table);
+
+    Array.from(table.rows).forEach((row) => {
+      // row.insertCell(1) erzeugt eine neue <td> an Spaltenindex 1
+      const newCell = row.insertCell(1);
+      // Optional: Inhalt oder Attribute setzen
+      newCell.textContent = "xx"; // z.B. leer
+      // newCell.classList.add('meine-klasse');
+    });
   };
 
   plugin.server_startup = function () {
