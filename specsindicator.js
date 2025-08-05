c3f1dc3b762070a090093084d88be0d1d583ff1c
Rayk Jackisch <rayk@jackisch.net>
2025-08-05 08:48:54 +0200

    Update version to 0.0.11 in config.json and refactor specsindicator.js

--- specsindicator.js
+++ specsindicator.js
@@ -1,31 +1,12 @@
-/**
- * @description MeshCentral Sample Plugin
- * @author Ryan Blenis
- * @copyright
- * @license Apache-2.0
- * @version v0.0.1
- */
+// consoleLogger.js
+var plugin = {};
 
-"use strict";
-
-module.exports.sample = function (parent) {
-  var obj = {};
-  obj.parent = parent; // keep a reference to the parent
-  obj.exports = [
-    "onDesktopDisconnect",
-    "onWebUIStartupEnd" // export this function to the web UI
-  ];
-
-  obj.onDesktopDisconnect = function () {
-    // this is called when the desktop disconnect button is clicked
-    writeDeviceEvent(encodeURIComponent(currentNode._id)); // mimic what the button does on the device main page to pull up a log
-    Q("d2devEvent").value = Date().toLocaleString() + ": "; // pre-fill the date for a timestamp
-    focusTextBox("d2devEvent");
-  };
-
-  obj.onWebUIStartupEnd = function () {
-    console.log("moep");
-  };
-
-  return obj;
+// Dieser Hook läuft im Browser, sobald die UI fertig geladen ist
+plugin.onWebUIStartupEnd = function () {
+  console.log("✅ ConsoleLogger-Plugin geladen!");
 };
+
+// Exportiere den Hook in die Web-UI-Ebene
+plugin.exports = ["onWebUIStartupEnd"];
+
+module.exports = plugin;
