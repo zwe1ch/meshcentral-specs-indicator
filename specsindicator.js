772b370c8c012cf112d55b784f0463e034c43252
Rayk Jackisch <rayk@jackisch.net>
2025-08-05 09:43:25 +0200

    Update version to 0.0.14 in config.json and modify version references in specsindicator.js

--- specsindicator.js
+++ specsindicator.js
@@ -4,19 +4,21 @@ module.exports.specsindicator = function (parent) {
   plugin.meshServer = parent.parent;
   plugin.db = null;
 
-  const ver = 13;
   plugin.exports = ["onWebUIStartupEnd", "goPageStart", "goPageEnd"];
 
   plugin.onWebUIStartupEnd = function () {
+    const ver = 14;
     console.log("onWebUIStartupEnd: " + ver);
   };
 
   plugin.goPageStart = function (index, event) {
+    const ver = 14;
     console.log("goPageStart: " + ver);
     console.log("index: " + index);
   };
 
   plugin.goPageEnd = function (index, event) {
+    const ver = 14;
     console.log("goPageEnd: " + ver);
     console.log("index: " + index);
   };
